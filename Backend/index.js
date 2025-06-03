8a99c432fc75228b1a0e8aac75b5991a9fe27bc3
abhishek <220104003@hbtu.ac.in>
2025-06-03 12:57:46 +0530

    changed

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv';
-dotenv.config({});
 import connectDB from "./DataBase/db.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./FormRoutes/userRoute.js";
 import postRoute from "./FormRoutes/postRoute.js"
 import cors from "cors"
 
+dotenv.config({});
+connectDB();
 
 const app = express();
-const port = process.env.PORT || 8000;
-const URI = process.env.MONGO_URL;
 
-// middleware
-app.use(express.json()); 
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
+app.use("/pp/devcomp", userRoute);
+app.use("/pp/devcomp/post", postRoute);
 
-app.use("/pp/devcomp", userRoute); 
-app.use("/pp/devcomp/post" , postRoute)
-
-app.listen(port , (req , res)=>{      
-    console.log('App is running at port', port);
-    connectDB();
-    
-})
+export default app;
