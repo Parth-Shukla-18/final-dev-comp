c4cc643b13348293e78b6aa9db162969d60747f6
abhishek <220104003@hbtu.ac.in>
2025-06-07 11:05:50 +0300

    updated2

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,16 @@ const URI = process.env.MONGO_URL;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    // origin: [
+    //   "http://localhost:5173",
+    //   "https://devcomponent-flame.vercel.app"
+    // ],
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use("/pp/devcomp", userRoute);
 app.use("/pp/devcomp/post", postRoute);
